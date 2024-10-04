f23777cac83cfffbc0c8aaa119a3fe7f32456d2b
SeanCunningham-gmit <100633569+SeanCunningham-gmit@users.noreply.github.com>
2024-10-04 17:49:04 +0100

    Update index.js
    Added Commented Code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+// Import necessary libraries and components for the app
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+// Finding the root element in the HTML 
+// and set up React to render the app.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,4 +17,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: reportWebVitals is for measuring app performance,
 reportWebVitals();
