69d5c2bbda3aeb0753bbc67d623494fe37871873
SeanCunningham-gmit <100633569+SeanCunningham-gmit@users.noreply.github.com>
2024-10-04 17:56:51 +0100

    Update NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,8 +1,10 @@
+// Import the Navbar and Nav components
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+// Defining a component called NavigationBar
 const NavigationBar = () => {
   return (
+    // Created a Bootstrap-styled navbar with a light background
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="#">React App</Navbar.Brand>
       <Nav className="mr-auto">
@@ -13,5 +15,5 @@ const NavigationBar = () => {
     </Navbar>
   );
 };
-
+// Export the NavigationBar component, so we can use it in other parts of the app
 export default NavigationBar;
