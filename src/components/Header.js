5d8f80f42c1c7b7683c65aa0d0b7eb5a402068aa
SeanCunningham-gmit <100633569+SeanCunningham-gmit@users.noreply.github.com>
2024-10-04 17:54:40 +0100

    Update Header.js
    Added commented code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
+// Defining a component called Header
 const Header = () => {
+   // Creating an h1 element 
+  //  using bootstraps "text-center" class to center the text
   return <h1 className="text-center">My Header in another component</h1>;
 };
-  
+  // Export the Header, so we can use it in other parts of the app
   export default Header;
