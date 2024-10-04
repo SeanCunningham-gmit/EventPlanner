feecd1b55ca1e8283944c103115fc468dd07db01
SeanCunningham-gmit <100633569+SeanCunningham-gmit@users.noreply.github.com>
2024-10-04 17:51:24 +0100

    Update Content.js
    Added commented code

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,14 @@
+// Import the React library
 import React from 'react';
-
+// Defining a component called Content
 const Content = () => {
   return (
+    // Creating a div containing two headings
     <div>
       <h1>Hello World!</h1>
       <h2>It is {new Date().toLocaleTimeString()}</h2>
     </div>
   );
 }
-
+// Export the Content component, this allows use to use it in other parts of the app
 export default Content;
